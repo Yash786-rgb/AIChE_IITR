120eb55c60508ce4a06220599003d219bcb263fe
prateekchandra <prateekchandra1027@gmail.com>
2021-11-28 22:10:01 +0530

    Added redirect

---
+++ src/components/Activities/Activities.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Navbar from '../Navbar/Navbar.jsx';
+
+import Footer from "../Footer/Footer.jsx";
+
+
+export default class Activities extends React.PureComponent{
+    render(){
+        return(
+            <div>
+            <br/>
+            <br/>
+            <h1 style = {{"height" : "250px"}}   className = "text-center">Under construction</h1>
+            </div>
+        )
+    }
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import Contact_us_Modal from "../Contact_us_modal/Contact_us_modal.jsx";
+
+
+import {Link} from "react-router-dom";
+
+
+class NavigationBar extends React.Component {
+
+    render() {
+        const routes = [
+            "Home",
+            "Blogs",
+            "Activities",
+            "Team"
+        ]
+
+        return (
+            <div className="Navbar">
+                <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+                    <a className="navbar-brand" href="/"
+                    >
+                        <img
+                            alt=""
+                            src={`../AIChE_logo.png`}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                    &nbsp;AIChE IIT Roorkee </a
+                    >
+                    <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ml-auto" >
+                            {routes.map(r => 
+                                {
+                                if (this.props.active == r) {
+                                    return (
+                                        <li className="nav-item active">
+                                            {r != "Home" && <Link class="nav-link" to={"/" + r}>{r}</Link>}
+                                            {r == "Home" && <Link class="nav-link" to={"/"}>{r}</Link>}
+                                        </li>
+                                    )
+                                }
+                                else {
+                                    return (
+                                        <li className="nav-item">
+                                        {r != "Home" && <Link class="nav-link" to={"/" + r}>{r}</Link>}
+                                        {r == "Home" && <Link class="nav-link" to={"/"}>{r}</Link>}
+                                        </li>
+                                    )
+                                }
+                            })}
+                            <a style = {{"color" : "rgb(156 148 148)", "marginTop" : "9px"}} href="#myModal" role="button" data-toggle="modal">Contact Us</a>
+                        </ul>
+                    </div>
+                </nav>
+                <Contact_us_Modal/>
+            </div>
+        )
+    }
+}
+
+
+export default NavigationBar
